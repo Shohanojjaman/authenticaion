3164601f1fcf2b039eaa37d672579ce083d92041
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-30 19:27:22 +0600

    LInked Signin and signup to home page.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,13 +7,23 @@ const Home = () => {
       <Helmet>
         <title>Home | Authentication</title>
       </Helmet>
-      <div className="flex flex-col justify-center items-center h-screen gap-5">
+      <div className="flex flex-col justify-center items-center h-screen gap-5 from-bg text-white">
         <h1 className="text-5xl">Home</h1>
-        <Link to='/login'>
-          <button className="border px-4 py-2 rounded-lg bg-green-400 hover:bg-green-600 font-bold">
-            Click to Signin
-          </button>
-        </Link>
+
+        <div className="flex gap-6">
+          <div className="text-center">
+            <p>Do you have account?</p>
+            <Link to="/signin" className="mt-3">
+              <button className="border px-4 py-2 rounded-lg bg-green-400 hover:bg-green-600 font-bold">Sign In</button>
+            </Link>
+          </div>
+          <div className="text-center">
+            <p>Don&apos;t you have account?</p>
+            <Link to="/signup">
+              <button className="border px-4 py-2 rounded-lg bg-green-400 hover:bg-green-600 font-bold">Sign Up</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
