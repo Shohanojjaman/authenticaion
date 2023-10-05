e976d28b58d0e5fcc1ab966bc1ec02253e40f641
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-05 11:12:30 +0600

    Added AuthProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
+import AuthProvider from './Components/AuthProvider';
 import MainRoutes from './Routes/MainRoutes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={MainRoutes}></RouterProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={MainRoutes}></RouterProvider>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
